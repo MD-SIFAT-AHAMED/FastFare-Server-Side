d6701c285240631ca6c748ad9eb5080347716343
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-07-01 16:11:15 +0600

    initial setup and mogodb conected

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sfxielf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const parcelCollection = client.db("parcelDB").collection("parcels");
+
+    app.get("/parcels", async (req, res) => {
+      const parcels = await parcelCollection.find().toArray();
+      res.send(parcels);
+    });
+
+    app.post("/parcels", async (req, res) => {
+      try {
+        const newParcel = req.body;
+        const result = await parcelCollection.insertOne(newParcel);
+        res.status(201).send(result);
+      } catch (err) {
+        console.error("Error insertig parcel:", err);
+        res.status(500).send({ message: "Failed to create parcel" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("FastFare Server is running...");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
